c89c7489581083274a3717acfdd68d4a40500aee
sidaodomorro <danilaferrari77@gmail.com>
2026-07-17 18:25:06 -0300

    Add default.box64rc from Winlator 11.1

--- app/src/main/java/com/winlator/cmod/xenvironment/components/GuestProgramLauncherComponent.java
+++ app/src/main/java/com/winlator/cmod/xenvironment/components/GuestProgramLauncherComponent.java
@@ -150,11 +150,19 @@ public class GuestProgramLauncherComponent extends EnvironmentComponent {
         synchronized (lock) {
             if (wineInfo.isArm64EC())
                 extractEmulatorsDlls();
-            else
+            else {
                 extractBox64Files();
+                copyDefaultBox64RCFile();
+            }
             checkDependencies();
             pid = execGuestProgram();
         }
+    }
+
+    private void copyDefaultBox64RCFile() {
+        Context context = environment.getContext();
+        File rootDir = environment.getImageFs().getRootDir();
+        FileUtils.copy(context, "box64/default.box64rc", new File(rootDir, "/usr/etc/config.box64rc"));
     }
 
 
@@ -430,7 +438,9 @@ public class GuestProgramLauncherComponent extends EnvironmentComponent {
 
         envVars.putAll(Box64PresetManager.getEnvVars("box64", environment.getContext(), box64Preset));
         envVars.put("BOX64_X11GLX", "1");
-        envVars.put("BOX64_NORCFILES", "1");
+
+        File box64RCFile = new File(environment.getImageFs().getRootDir(), "/usr/etc/config.box64rc");
+        envVars.put("BOX64_RCFILE", box64RCFile.getPath());
     }
 
     public void suspendProcess() {
